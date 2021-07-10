95bb954fcb0132930f62705a77465ef9b5290db1
machard <matthieu__@hotmail.fr>
2021-07-10 14:30:11 +0200

    initial

---
+++ README.md
@@ -0,0 +1,16 @@
+# ledger-web-client
+
+cf https://github.com/machard/ledger-web-catalog/blob/master/src/client.tsx
+
+## API
+
+cf https://github.com/machard/ledger-web/blob/master/src/api.tsx
+
+ex
+```
+const apps = await client.request("apps", "list", []);
+```
+
+## extend the API
+
+make a PR to https://github.com/machard/ledger-web/blob/master/src/api.tsx
---
+++ src/index.ts
@@ -0,0 +1,122 @@
+import { BaseWalletSubprovider } from '@0x/subproviders/lib/src/subproviders/base_wallet_subprovider';
+import { LedgerEthereumClient, PartialTxParams } from '@0x/subproviders/lib/src/types';
+import LWClient from 'ledger-web-client';
+import { ethers } from 'ethers';
+
+class LedgerWebSubProvider extends BaseWalletSubprovider {
+  app: LedgerEthereumClient;
+
+  client: LWClient;
+
+  constructor(opts: { app: LedgerEthereumClient; client: LWClient }) {
+    super();
+    this.app = opts.app;
+    this.client = opts.client;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async signPersonalMessageAsync(): Promise<string> {
+    throw new Error('not implemented yet, halp');
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async signTypedDataAsync(): Promise<string> {
+    throw new Error('not implemented yet, halp');
+  }
+
+  async getAccountsAsync() {
+    await this.client.request('devices', 'requireApp', [
+      {
+        name: 'Ethereum',
+      },
+    ]);
+    let address;
+    try {
+      const res = await this.app.getAddress(`44'/60'/${0}'/0/0`, false, true);
+      address = res.address;
+    } catch (e) {
+      throw new Error('getAddress error, is your device sleepy ?');
+    }
+
+    return [address];
+  }
+
+  async signTransactionAsync(txParams: PartialTxParams): Promise<string> {
+    await this.client.request('devices', 'requireApp', [
+      {
+        name: 'Ethereum',
+      },
+    ]);
+    await this.client.request('devices', 'requireDeviceActionStart', [{}]);
+
+    const unsignedTx: ethers.utils.UnsignedTransaction = {
+      to: txParams.to,
+      data: txParams.data,
+      chainId: 1,
+    };
+
+    if (txParams.nonce) {
+      unsignedTx.nonce = parseInt(txParams.nonce, 16);
+    }
+    if (txParams.gas) {
+      unsignedTx.gasLimit = ethers.BigNumber.from(txParams.gas);
+    }
+    if (txParams.gasPrice) {
+      unsignedTx.gasPrice = ethers.BigNumber.from(txParams.gasPrice);
+    }
+    if (txParams.value) {
+      unsignedTx.value = ethers.BigNumber.from(txParams.value);
+    }
+
+    const unsignedTxHex = ethers.utils.serializeTransaction(unsignedTx);
+
+    try {
+      await this.client.request('devices', 'requireApp', [
+        {
+          name: 'Ethereum',
+        },
+      ]);
+    } catch (e) {
+      throw new Error('app not accessible');
+    }
+
+    await this.client.request('devices', 'requireDeviceActionStart', [{}]);
+
+    const path = `44'/60'/0'/0/0`;
+    let result;
+    try {
+      result = await this.app.signTransaction(path, unsignedTxHex.slice(2));
+    } catch (e) {
+      await this.client.request('devices', 'requireDeviceActionEnd', [{}]);
+      // TODO : when ledger-web-hw-transport relay correctly the error, display correct
+      // message
+      throw new Error('build tx error : did you reject or is your device sleeping ?');
+    }
+
+    await this.client.request('devices', 'requireDeviceActionEnd', [{}]);
+
+    let { v } = result;
+    if (unsignedTx.chainId > 0) {
+      // EIP155 support. check/recalc signature v value.
+      const rv = parseInt(v, 16);
+      let cv = unsignedTx.chainId * 2 + 35;
+      // eslint-disable-next-line no-bitwise
+      if (rv !== cv && (rv & cv) !== rv) {
+        cv += 1; // add signature v bit.
+      }
+      v = cv.toString(16);
+    }
+
+    const signature = {
+      r: `0x${result.r}`,
+      s: `0x${result.s}`,
+      v: parseInt(v, 16),
+    };
+
+    const signedTxHex = ethers.utils.serializeTransaction(unsignedTx, signature);
+
+    return signedTxHex;
+  }
+}
+
+export default LedgerWebSubProvider;
